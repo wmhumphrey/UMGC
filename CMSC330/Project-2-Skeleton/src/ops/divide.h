8334b967afd28a3c5897c36cc59a66b2ddb9b3c7
wmhumphrey <wyatt@humphreyfamily.me>
2025-10-03 15:34:41 -0400

    Add CMSC330 Project 2

---
+++ CMSC330/Project-2-Skeleton/src/ops/divide.h
@@ -0,0 +1,9 @@
+
+class Divide: public SubExpression {
+public:
+    Divide(Expression* left, Expression* right): SubExpression(left, right) {
+    }
+    double evaluate() {
+       return left->evaluate() / right->evaluate();
+    }
+};
---
+++ CMSC330/Project-2-Skeleton/src/ops/exponent.h
@@ -0,0 +1,10 @@
+#include <cmath>
+
+class Exponent: public SubExpression {
+public:
+    Exponent(Expression* left, Expression* right): SubExpression(left, right) {
+    }
+    double evaluate() {
+       return pow(left->evaluate(), right->evaluate());
+    }
+};
