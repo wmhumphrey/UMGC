ea1c9bfe5a45e16dff6dc39dea236965e1008d77
wmhumphrey <wyatt@humphreyfamily.me>
2025-10-06 11:28:26 -0400

    fix avg

--- CMSC330/Project-2-Skeleton/src/ops/avg.h
+++ CMSC330/Project-2-Skeleton/src/ops/avg.h
@@ -1,9 +1,9 @@
-#include <algorithm>
+
 
 class Avg: public SubExpression {
-public: 
-    Avg(Expression* left, Expression* right): SubExpression(left, right){}
+public:
+    Avg(Expression *left, Expression *right) : SubExpression(left, right) {}
     double evaluate(){
-        return std::mean(left->evaluate(), right->evaluate());
+        return (left->evaluate() + right->evaluate()) / 2.0;
     }
 };
