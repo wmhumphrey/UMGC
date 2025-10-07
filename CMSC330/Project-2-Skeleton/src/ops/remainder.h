0ec68380579531b98c99ba19fcc2cb2f25397da5
wmhumphrey <wyatt@humphreyfamily.me>
2025-10-07 11:54:35 -0400

    fix remainder

--- CMSC330/Project-2-Skeleton/src/ops/remainder.h
+++ CMSC330/Project-2-Skeleton/src/ops/remainder.h
@@ -1,9 +1,10 @@
+#include <cmath>
 
 class Remainder: public SubExpression {
 public:
     Remainder(Expression* left, Expression* right): SubExpression(left, right) {
     }
     double evaluate() {
-       return left->evaluate() % right->evaluate();
+       return fmod(left->evaluate(), right->evaluate());
     }
 };
